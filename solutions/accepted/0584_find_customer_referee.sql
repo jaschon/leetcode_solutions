f5278b81ae414ff09ba5062e78b11b4b73254993
jaschon <jason.rebuck@gmail.com>
2022-05-26 10:38:52 -0700

    add 584, 1757

---
+++ solutions/accepted/0584_find_customer_referee.sql
@@ -0,0 +1,14 @@
+# 584. Find Customer Referee
+# Easy
+
+# Accepted
+# Result: https://leetcode.com/submissions/detail/707784409/
+# Time: 820ms (8.76%)
+# Mem: 0
+
+
+
+
+# Write your MySQL query statement below
+select name from Customer where referee_id <> 2 or referee_id is null
+
---
+++ solutions/accepted/1757_recyclable_and_lowfat.sql
@@ -0,0 +1,14 @@
+# 1757. Recyclable and Low Fat Products
+# Easy
+
+# Accepted
+# Result: https://leetcode.com/submissions/detail/707776985/
+# Time: 881ms (9.09%)
+# Mem: 0
+
+# Write your MySQL query statement below
+select product_id from Products
+where recyclable = "Y" and low_fats = "Y"
+
+
+
